69b873a92f6d7437e927cdeb712c001594f73f08
Joakim Karlsson <joakim@jkarlsson.com>
2014-07-13 17:27:52 +0200

    Somewhat better test names

--- specs/describe_it_spec.py
+++ specs/describe_it_spec.py
@@ -12,12 +12,12 @@ def an_outer_context():
         fixture.x = 0
 
     @it
-    def should_do_something():
+    def it_uses_the_value_initialized_in_before_each():
         assert fixture.x == 0
         fixture.x = 12
 
     @it
-    def should_do_something_else():
+    def it_doesnt_let_tests_interfere_with_each_other():
         assert fixture.x == 0
         fixture.x = 13
 
@@ -25,9 +25,8 @@ def an_outer_context():
     def an_inner_context():
 
         @it
-        def should_do_something():
+        def it_picks_up_the_outer_contexts_setup():
             assert fixture.x == 0
-
 
     @describe
     def augments_fixture():
@@ -48,5 +47,5 @@ def an_outer_context():
                 fixture.x += 10
 
             @it
-            def should_work():
+            def gets_the_augmented_fixture():
                 assert fixture.x == 32
