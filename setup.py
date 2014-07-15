5b1b9bde7ece42d225a5cb532a19b152d2259f10
Joakim Karlsson <joakim@jkarlsson.com>
2014-07-15 10:41:42 +0200

    Version 0.1.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='describe_it',
-    version='0.1.3',
+    version='0.1.4',
     description=('A nose plugin that supports writing describe/it style'
                  'unit tests with nested contexts'),
     long_description=open('README.rst').read(),
